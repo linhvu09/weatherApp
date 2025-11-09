d5dd769d67dd61a2ef64f6dd164c3625000adc10
DucPhong08 <phongdd.b23cc127@stu.ptit.edu.vn>
2025-11-09 21:58:59 +0700

    Update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
